1df508098b8bce58077b4f13d9da988c7d63007d
RobertttBS <k0983357798@gmail.com>
2024-02-15 21:19:40 +0800

    Add a L3 translation table (pte).

--- include/mm.h
+++ include/mm.h
@@ -35,7 +35,6 @@
 #define PD_ACCESS                       (1 << 10)
 
 #define BOOT_PGD_ATTR                   (PD_TABLE)
-#define BOOT_PUD_ATTR                   (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << 2) | PD_BLOCK)
 #define DEVICE_BLOCK_ATTR               (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << 2) | PD_BLOCK)
 #define NORMAL_BLOCK_ATTR               (PD_ACCESS | (MAIR_IDX_NORMAL_NOCACHE << 2) | PD_BLOCK)
 #define DEVICE_PAGE_ATTR                (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << 2) | PD_PAGE)
@@ -48,5 +47,7 @@
 #define MMU_PUD_ADDR                    0x1000
 #define MMU_PMD_ADDR                    0x2000
 #define MMU_PTE_ADDR                    0x3000
+
+#define PERIPH_MMIO_BASE                0x3F000000
 
 #endif
